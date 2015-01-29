732603c330f9ac474211b64ee1a11597f4452cc2
Ramses <ramses@oncircle.com>
2015-01-28 22:41:11 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,1 @@
-$("#main").append([RamsesRosas]); where [RamsesRosas] should be replaced with your name in quotes. For example $("#main").append("Charlie Chaplin");.
+$("#main").append(RamsesRosas); where RamsesRosas should be replaced with your name in quotes. For example $("#main").append("Charlie Chaplin");.
